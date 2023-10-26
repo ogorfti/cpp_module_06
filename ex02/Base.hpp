8ba105d0e8c994a8cccaf42d1b395ffc1a9a00ab
Oussama Gorfti <ogorfti@c1r3p4.1337.ma>
2023-10-26 13:07:01 +0100

    add some info

--- ex02/Base.hpp
+++ ex02/Base.hpp
@@ -6,7 +6,7 @@
 /*   By: ogorfti <ogorfti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 17:10:41 by ogorfti           #+#    #+#             */
-/*   Updated: 2023/10/26 10:25:29 by ogorfti          ###   ########.fr       */
+/*   Updated: 2023/10/26 11:55:24 by ogorfti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 #include <iostream>
 
+//* If there is a surety we will never cast to wrong object
+//* then always avoid dynamic_cast and use static_cast. 
 class Base
 {
 	public:
